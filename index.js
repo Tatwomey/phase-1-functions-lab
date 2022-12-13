a8693193993449da2062674058da40d067a5af5a
Trevor Twomey <trevor.a.twomey@gmail.com>
2022-12-13 12:10:13 -0500

    commit tree 29c117533053b84c6dba3dacb77ce4bcca0002d4

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+
+const distanceFromHqInBlocks = (location) => {
+return Math.abs(location - 42)
+}
+  const theDistance = distanceFromHqInBlocks(34)
+  
+  function distancerFromHqInFeet(location){
+   return distancefromHqInBlocks(location) * 264
+  }
+  function distancerFromHqInFeet(start, destination){
+const distance = Math.abs(start - destination)
+return distance * 264
+  }
